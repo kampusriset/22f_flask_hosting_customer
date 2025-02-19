d5f00ac00befc7f28b2be0c3b657d8af683b6274
nuciii <iniikhsan3101@gmail.com>
2025-02-19 20:02:00 +0700

    Create ikhsan.py

---
+++ Team/ikhsan.py
@@ -0,0 +1,11 @@
+def perkenalan(nama, nim, peran):
+    print("Halo! Perkenalkan, nama saya", nama)
+    print("NIM : ", nim)
+    print("Peran saya yaitu : ", peran)
+
+if __name__ == "__main__":
+    nama = "Khairul Ikhsan"
+    nim = "22.130.10.356"
+    peran = "Presentasi Review Website (Presenter)"
+    perkenalan(nama, nim, peran)
+    
