8375e45c87cbb674aa97978e3f545e7c6236873d
RomizAisy <169019656+RomizAisy@users.noreply.github.com>
2025-02-19 05:51:55 -0800

    Create romiz.py

---
+++ Team/romiz.py
@@ -0,0 +1,10 @@
+def perkenalan(nama, nim, peran):
+    print("Halo! Perkenalkan, nama saya", nama)
+    print("NIM : ", nim)
+    print("Peran saya yaitu : ", peran)
+
+if __name__ == "__main__":
+    nama = "R. Romiz Aisy"
+    nim = "22.130.10.377"
+    peran = "Sebagai Ide dan Pengembangan"
+    perkenalan(nama, nim, peran)
