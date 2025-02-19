0d0c5b750926be3e40b954c22bae52c0609ae706
Mas Mbelo <117719643+thehashslinging@users.noreply.github.com>
2025-02-19 19:57:51 +0700

    add team

---
+++ Team/akbar.py
@@ -0,0 +1,11 @@
+def perkenalan(nama, nim, peran):
+    print("Halo! Perkenalkan, nama saya", nama)
+    print("NIM : ", nim)
+    print("Peran saya yaitu : ", peran)
+
+if __name__ == "__main__":
+    nama = "Akbar Rizky Dio Saputra"
+    nim = "22.130.10.355"
+    peran = "Mengerjakan Script Flask (Developer)"
+    perkenalan(nama, nim, peran)
+    
