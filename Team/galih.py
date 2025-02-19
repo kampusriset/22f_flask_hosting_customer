57f4f86534d1d900880697eae3812c5125e1b867
GalihArfn001 <galiharfn01@gmail.com>
2025-02-19 20:09:27 +0700

    Create galih.py

---
+++ Team/galih.py
@@ -0,0 +1,11 @@
+def perkenalan(nama, nim, peran):
+    print("Halo! Perkenalkan, nama saya", nama)
+    print("NIM : ", nim)
+    print("Peran saya yaitu : ", peran)
+
+if __name__ == "__main__":
+    nama = "Muhammad Galih Arifin"
+    nim = "22.130.10.368"
+    peran = "Mengkoordinasi seluruh anggota tim (Leader)"
+    perkenalan(nama, nim, peran)
+    
